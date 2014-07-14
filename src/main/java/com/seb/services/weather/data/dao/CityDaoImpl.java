7e11b393239253fa7959a921f77a1b32d6ea0a57
s.vandamme <sebastien.vandamme@gmail.com>
2014-07-14 20:46:55 +0200

    Spring + hibernate dependencies

--- src/main/java/com/seb/services/weather/data/dao/CityDaoImpl.java
+++ src/main/java/com/seb/services/weather/data/dao/CityDaoImpl.java
@@ -1,11 +1,16 @@
 package com.seb.services.weather.data.dao;
 
 import com.seb.services.weather.data.orm.City;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by sebastien.vandamme@gmail.com on 12/07/2014.
  */
+@Repository
 public class CityDaoImpl implements CityDao {
+
+    private Logger LOGGER = Logger.getLogger(CityDaoImpl.class);
 
     protected CityDaoImpl() {
         super();
